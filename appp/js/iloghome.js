900dec62fd548c75492fd2313d72f66b10f87422
gloria-k-yeboah <gabky100@yahoo.com>
2014-12-04 16:51:39 +0000

    added the burn-up chart

--- appp/js/iloghome.js
+++
@@ -1,7 +0,0 @@
-/*
-'use strict';
-var iLogAppController = angular.module('iLogAppController', []);
-
-iLogAppController.controller('HomeCtrl', ['$scope', function($scope){
-    $scope.header = 'Log an issue';
-}]);*/
--- appp/js/viewlog.js
+++
@@ -1,5 +0,0 @@
-var iLogAppController = angular.module('iLogAppController', []);
-
-iLogAppController.controller('ViewCtrl', ['$scope', function($scope){
-    $scope.header = "View an issue";
-}]);
